2ff1f51f24da6512ce791d3d65724a20bd2d3e15
tevdevelops <tevinrivera94@gmail.com>
2019-03-06 00:46:51 -0500

    Created API views for user authentication

--- leadmanager/frontend/src/actions/leads.js
+++ leadmanager/frontend/src/actions/leads.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { createMessage } from "./messages";
+import { createMessage, returnErrors } from "./messages";
 
 import { GET_LEADS, DELETE_LEAD, ADD_LEAD, GET_ERRORS } from "./types";
 
@@ -13,7 +13,9 @@ export const getLeads = () => dispatch => {
         payload: response.data
       });
     })
-    .catch(error => console.log(error));
+    .catch(error =>
+      dispatch(returnErrors(error.response.data, error.response.status))
+    );
 };
 
 // DELETE LEAD
@@ -41,14 +43,7 @@ export const addLead = lead => dispatch => {
         payload: response.data
       });
     })
-    .catch(error => {
-      const errors = {
-        msg: error.response.data,
-        status: error.response.status
-      };
-      dispatch({
-        type: GET_ERRORS,
-        payload: errors
-      });
-    });
+    .catch(error =>
+      dispatch(returnErrors(error.response.data, error.response.status))
+    );
 };
--- leadmanager/leadmanager/settings.py
+++ leadmanager/leadmanager/settings.py
@@ -37,10 +37,16 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'rest_framework',
     'leads',
-    'frontend'
+    'rest_framework',
+    'frontend',
+    'knox',
+    'accounts'
 ]
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': ('knox.auth.TokenAuthentication',)
+}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
